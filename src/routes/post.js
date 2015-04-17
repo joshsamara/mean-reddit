ef7d34c5413bd91e9d5d2dbba5a4a501b02110c9
Josh Samara <joshsamara@gmail.com>
2015-04-17 16:08:21 -0400

    Basic Post api.

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,9 +1,33 @@
 module.exports = function(app, models){
 
-    app.get('/api/post', function(req, res){
+    // Useful because we do this more than once
+    function returnall(req, res){
         models.Post.find(function(err, data){
             res.json(data);
         });
+    }
+
+    app.get('/api/post', function(req, res){
+        returnall(req, res);
+    });
+
+    app.post('/api/post/', function(req, res){
+        post = models.Post(req.body);
+        post.save();
+        res.json(post);
+    });
+
+    app.get('/api/post/:id', function(req, res){
+        var id = req.params.id;
+        models.Post.findById(id, function(err, data){
+            res.json(data);
+        });
+    });
+
+    app.delete('/api/post/:id', function(req, res){
+        var id = req.params.id;
+        function callback() {returnall(req, res); }
+        models.Post.findByIdAndRemove(id , callback);
     });
 
 };
