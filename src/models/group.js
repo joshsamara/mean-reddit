4224a06f31de5c4f3833cb00a863235325b3cd5d
Josh Samara <joshsamara@gmail.com>
2015-04-17 22:41:41 -0400

    Fixes to the group and post scheam.

--- src/models/group.js
+++ src/models/group.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GroupSchema = new Schema({
-    name: { type: String, index: { unique: true }},
+    name: { type: String, index: { unique: true }, required: true },
     owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     created: { type: Date, default: Date.now, required: true }
 });
--- src/models/post.js
+++ src/models/post.js
@@ -4,11 +4,22 @@ var Schema = mongoose.Schema;
 var PostSchema = new Schema({
     title: { type: String, required: true },
     link_post: { type: Boolean, required: true },
+    link: { type: String },
     text: { type: String },
     group: { type: Schema.Types.ObjectId, ref: 'Group', required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     score: { type: Number, required: true, default: 0},
     created: { type: Date, default: Date.now, required: true }
+});
+
+// Conditional validation
+PostSchema.pre('validate', function(next) {
+    if (this.link_post  && !this.link) {
+        this.invalidate('link', 'Link posts require a link');
+    } else if (!this.link_post && this.link){
+        this.invalidate('link', 'Text posts cannot have a link');
+    }
+    next();
 });
 
 module.exports = mongoose.model('Post', PostSchema);
