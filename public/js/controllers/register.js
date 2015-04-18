19b52fe1b21b401f1bb387f42b530e71e6d4bd47
Josh Samara <joshsamara@gmail.com>
2015-04-18 04:37:38 -0400

    Fix duplicate use of .errors.

--- public/js/controllers/register.js
+++ public/js/controllers/register.js
@@ -4,12 +4,12 @@ angular.module("MainApp").controller('RegisterController', ['$scope', '$http', '
         // Allow logging in
         $scope.register = function(form) {
             $http.post("/register", form).success(function(response){
-                $scope.error = null;
+                $scope.regerror = null;
                 userFactory.set(response);
                 $scope.refreshUser()
                 $location.path('/');
             }).error(function(response){
-                $scope.error = "Invalid username or password.";
+                $scope.regerror = "Invalid username or password.";
             });
         };
 }]);
