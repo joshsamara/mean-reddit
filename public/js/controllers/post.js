b04be91e77a540cfb5e286f4f8b80be423758213
Josh Samara <joshsamara@gmail.com>
2015-04-18 11:19:09 -0400

    Basic post view.

--- public/js/controllers/post.js
+++ public/js/controllers/post.js
@@ -1,4 +1,22 @@
 // Controller for viewing a post
-angular.module("MainApp").controller('PostController', ['$scope', '$http', 'userFactory',
-    function($scope, $http, userFactory) {
+angular.module("MainApp").controller('PostController', ['$scope', '$http', 'userFactory', '$routeParams',
+    function($scope, $http, userFactory, $routeParams) {
+        var postId = $routeParams.postId;
+        var user = userFactory.get();
+        // Get our post
+        $http.get("/api/post/" + postId).success(function(response) {
+            $scope.post = response;
+            $scope.owner = user && (user._id === $scope.post.user);
+            $http.get("/api/group/" + $scope.post.group).success(function(response){
+                $scope.group = response;
+            });
+            if (!$scope.owner){
+                $http.get("/api/user/" + $scope.post.user).success(function(response){
+                    $scope.poster = response;
+                });
+            }
+        }).error(function(response){
+            $location.path('/404');
+        });
+
 }]);
