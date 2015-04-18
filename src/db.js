f4f6a0cf2c57e140669f8c90a13c39e071568cca
Josh Samara <joshsamara@gmail.com>
2015-04-18 11:53:22 -0400

    Use openshift db url if available.

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 // Put all our db config here
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/db');
+db_url = process.env.OPENSHIFT_MONGO_DB_URL || 'mongodb://localhost/';
+db_url = db_url + 'readitdb';
+mongoose.connect(db_url);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
